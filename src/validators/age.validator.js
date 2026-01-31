4aa1c62f40bbab6d264b586a1df70bebdd4512bb
polianabrandao <polibarross@gmail.com>
2026-01-31 18:30:07 -0300

    feat: implement age validation logic in age.validator.js #7

---
+++ src/validators/age.validator.js
@@ -0,0 +1,17 @@
+const ageValidator = (age) => {
+  if (age === null || age === undefined) {
+    return false;
+  }
+
+  if (typeof age !== 'number') {
+    return false;
+  }
+
+  if (age < 18 && age > 120) {
+    return false;
+  }
+
+  return true;
+};
+
+export default ageValidator;
