4b932350d4fc7c333a87ca28427830a7889077c3
Marcelo Gonçalves <marcelojunior_@outlook.com.br>
2026-01-31 20:08:36 -0300

    refactor: improve formatting and consistency in full name validation logic #16

--- src/validators/fullName.validator.js
+++ src/validators/fullName.validator.js
@@ -1,25 +1,25 @@
-const fullNameValidator = (name) => { 
-    // nome: Marcelo Gonçalves de Oliveira Júnior
-    // Verifica se o nome é uma string
-    if (typeof name !== 'string') {
-        return false;
-    }
-    
-    const parts = name.trim().split(/\s+/);
+const fullNameValidator = (name) => {
+  // nome: Marcelo Gonçalves de Oliveira Júnior
+  // Verifica se o nome é uma string
+  if (typeof name !== 'string') {
+    return false;
+  }
 
-    // Verifica se o nome contém pelo menos duas partes
-    if (parts.length < 2) {
-        return false;
-    }
+  const parts = name.trim().split(/\s+/);
 
-    // Verifica se o nome está vazio ou contém apenas espaços
-    for (const part of parts) {
-        if (part.length < 2 || !/^[A-Za-z]+$/.test(part)) {
-            return false;
-        }
+  // Verifica se o nome contém pelo menos duas partes
+  if (parts.length < 2) {
+    return false;
+  }
+
+  // Verifica se o nome está vazio ou contém apenas espaços
+  for (const part of parts) {
+    if (part.length < 2 || !/^[A-Za-z]+$/.test(part)) {
+      return false;
     }
-    
-    return true;
+  }
+
+  return true;
 };
 
 export default fullNameValidator;
