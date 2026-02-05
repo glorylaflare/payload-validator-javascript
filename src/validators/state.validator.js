db903a9cfe66a219455f564b4cf7aea8c096ad31
polianabrandao <polibarross@gmail.com>
2026-02-05 14:43:50 -0300

    feat: adição de validação de estados

---
+++ src/validators/state.validator.js
@@ -0,0 +1,18 @@
+import { STATES } from "../constants/states.constants"  
+
+const stateValidator = (state) => {
+    
+    if(state !== 'string'){
+        return false;
+    }
+    
+    for(const item of STATES){
+        if(state !== item){
+            return false;
+        }
+    }
+
+    return true;
+};
+
+export default stateValidator;
