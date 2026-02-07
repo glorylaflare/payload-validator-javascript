d61642a3a0677fe89081fd070519e7d6cf4b31a5
polianabrandao <polibarross@gmail.com>
2026-02-07 15:44:20 -0300

    feat: add phone validation logic #9

---
+++ src/validators/phone.validator.js
@@ -0,0 +1,20 @@
+const phoneValidator = (phone) => {
+    if(phone !== 'string'){
+        return false;
+    }
+
+    const phonePattern = /^(\(?\d{2}\)?\s?)?(9?\d{4}[-.\s]?\d{4})$/;
+    const matches = phonePattern.match(/\d{2}/);
+
+    if(!phonePattern.test(phone)){
+        return false;
+    }
+
+    if(matches < 11 || matches > 99){
+        return false;
+    }
+
+    return true;
+}
+
+export default phoneValidator;
