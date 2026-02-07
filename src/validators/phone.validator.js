0485bc2639b73ff09f4880e6794bb2fb78ae5e84
polianabrandao <polibarross@gmail.com>
2026-02-07 19:58:51 -0300

    fix: correct birth date format and improve code formatting in validators

--- src/validators/phone.validator.js
+++ src/validators/phone.validator.js
@@ -1,20 +1,20 @@
 const phoneValidator = (phone) => {
-    if(phone !== 'string'){
-        return false;
-    }
+  if (phone !== 'string') {
+    return false;
+  }
 
-    const phonePattern = /^(\(?\d{2}\)?\s?)?(9?\d{4}[-.\s]?\d{4})$/;
-    const matches = phonePattern.match(/\d{2}/);
+  const phonePattern = /^(\(?\d{2}\)?\s?)?(9?\d{4}[-.\s]?\d{4})$/;
+  const matches = phonePattern.match(/\d{2}/);
 
-    if(!phonePattern.test(phone)){
-        return false;
-    }
+  if (!phonePattern.test(phone)) {
+    return false;
+  }
 
-    if(matches < 11 || matches > 99){
-        return false;
-    }
+  if (matches < 11 || matches > 99) {
+    return false;
+  }
 
-    return true;
-}
+  return true;
+};
 
 export default phoneValidator;
